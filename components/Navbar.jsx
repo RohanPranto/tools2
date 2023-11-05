e88f2104aa73c9ff4f6a321c606bf2dfda3fe62a
Rohan Biswas <rohan.biswas2020@uem.edu.in>
2023-11-05 13:42:03 +0530

    navbar brand

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">Navbar</Link> {/* Use Link instead of anchor tag */}
+        <Link className="navbar-brand" to="/">Tools</Link> {/* Use Link instead of anchor tag */}
         <button
           className="navbar-toggler"
           type="button"
